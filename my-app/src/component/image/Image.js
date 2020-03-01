1ec4a44afdd2807c91f6a1261d3cf7b4f41258a8
bttodd11 <bttodd@icloud.com>
2020-02-29 19:59:28 -0600

    Have the array with all data and setState working

--- my-app/src/component/image/Image.js
+++ my-app/src/component/image/Image.js
@@ -4,13 +4,6 @@ import './Image.css';
 
 
 
-const API = 'https://gateway.marvel.com/v1/public/characters?';
-const pubKey = 'd3c4d49ca5140158b141102b27d684ae&';
-const hash = '9674d68e3057ba20fef81d98f535e7eb&limit=100';
-const date = '1&';
-const limit = '&limit=' + 100;
-let offset = 0;
-const offsetLimit = 10;
 const notAva = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available";
 
 
@@ -27,46 +20,57 @@ class Image extends React.Component {
       char: []
     };
 
-    const getData = (offset) =>{
+    function getData(params) {
       console.log("worked")
-    }
-
-   
   }
-
-  componentDidMount() {
-    fetch(API + 'ts=' + date + 'apikey=' + pubKey + 'hash=' + hash + limit + '&offset=' + offset)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            char: result.data.results,
-          });
-        }
-    ,
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-     this.getData(this.offset)
 }
+
+async componentDidMount() {
+const API = 'https://gateway.marvel.com/v1/public/characters?';
+const pubKey = 'd3c4d49ca5140158b141102b27d684ae&';
+const hash = '9674d68e3057ba20fef81d98f535e7eb&limit=100';
+const date = '1&';
+const limit = '&limit=' + 100;
+const offsetLimit = 10;
+let offset = 0;
+const url = API + 'ts=' + date + 'apikey=' + pubKey + 'hash=' + hash + limit + '&offset='
+// const response = await fetch(url)
+// const data = await response.json()
+let char = [];
+
+for (let index = 0; index < offsetLimit; index++) {
+  const response = await fetch(url + index * 100)
+  const data = await response.json()
+  char = char.concat(data.data.results);
+}
+
+this.setState({
+  isLoaded: true,
+  char: char,
+})
+
+console.log(char)
+  
+}
+
+ 
+ 
+
+  
+
 
 
   render() {
     const { error, isLoaded, char } = this.state;
-    const filteredData = char.filter(char => char.thumbnail.path !== notAva );
+    // const filteredData = char.filter(char => char.thumbnail.path !== notAva );
 
     console.log(char)
     return (
         <Container>
         <Row>
-          {filteredData.map(char => (<Col md="3"> 
+          {char.map(char => (<Col md="3"> 
           <p>{char.name}</p>
-          <img src={char.thumbnail.path + "/standard_large.jpg"} />{"\n"}
+          {/* <img src={char.thumbnail.path + "/standard_large.jpg"} />{"\n"} */}
           {/* <p>{char.description}</p> */}
           </Col>))}
 
